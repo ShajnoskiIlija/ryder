febf5df7ebf66e70568c1b8462dbb15578210afb
ShajnoskiIlija <shajnoskiilija@gmail.com>
2021-09-05 22:11:18 +0200

    Installation of Tailwind-full

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,31 @@
-module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
-      autoprefixer: {
-        flexbox: 'no-2009'
-      },
-      stage: 3
+let environment = {
+   plugins: [
+     require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),
+      require('postcss-import'),
+      require('postcss-flexbugs-fixes'),
+      require('postcss-preset-env')({
+        autoprefixer: {
+          flexbox: 'no-2009'
+        },
+        stage: 3
+      })
+    ]
+};
+
+if (procces.env.RAILS_ENV === 'production') {
+    environment.plugins.push
+    require('@fullhuman?postcss-purgecss')({
+      content: [
+        '.app/**/.html.erb',
+        '.app/helpers/**/*.rb',
+        '.app/javascript/**/*.js',
+
+
+      ]
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
     })
-  ]
-}
+
+  }
+
+module.exports = environment;
+
