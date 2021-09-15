db770fe85e6150a3b347d503a70c3b9f0fe28b8c
ShajnoskiIlija <shajnoskiilija@gmail.com>
2021-09-15 15:52:47 +0200

    specs using factory and added faker gem

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -2,13 +2,13 @@
 
 require 'rails_helper'
 
-RSpec.describe Contact, type: :model do
-  describe 'contact model spec' do
-    it 'does not let a user to leave empty field' do
-      contact = described_class.create!(full_name: 'test', email: 'goo@google.com', message: 'poraka')
-      expect(contact).to validate_presence_of(:full_name)
-      expect(contact).to validate_presence_of(:message)
-      expect(contact).to validate_presence_of(:email)
-    end
+describe Contact, type: :model do
+  subject(:contact) { create(:contact) }
+
+  describe 'validations' do
+    it { expect(contact).to be_valid }
+    it { expect(contact).to validate_presence_of(:full_name) }
+    it { expect(contact).to validate_presence_of(:email) }
+    it { expect(contact).to validate_presence_of(:message) }
   end
 end
