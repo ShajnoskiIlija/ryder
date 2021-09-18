d6d469cd9c497d7d834fecc32d5136cec74c9914
ShajnoskiIlija <shajnoskiilija@gmail.com>
2021-09-18 10:42:06 +0200

    changes on seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
-20.times do
+5.times do
     contact = Contact.create!(
-      username: Faker::Internet.username,
       full_name: Faker::Name.first_name,
       email: Faker::Internet.email,
       message: Faker::Lorem.paragraphs
