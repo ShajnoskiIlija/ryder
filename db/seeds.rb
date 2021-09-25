63993817f69091698ea8f8a1178774b767eca633
ShajnoskiIlija <shajnoskiilija@gmail.com>
2021-09-25 11:42:59 +0200

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,6 +3,18 @@
 require 'factory_bot_rails'
 include FactoryBot::Syntax::Methods
 
+puts 'Seed started...'
+
+puts 'Creating contacts...'
 5.times do
     create(:contact)
 end
+
+puts 'Creating users...'
+create(:user, email: 'user@test.com', first_name: 'Ilija', last_name: 'Shajnoski')
+
+10.times do
+     create(:user)
+end
+
+puts 'Seeding completed!'
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -6,7 +6,7 @@ FactoryBot.define do
     first_name { Faker::Name.first_name }
     last_name { Faker::Name.last_name }
     address { Faker::Address.street_address }
-    phone_number { Faker::PhoneNumber.phone_number }
+    phone_number { Faker::PhoneNumber.cell_phone }
     password { 'password' }
     password_confirmation { 'password' }
   end
