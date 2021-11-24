02f5c2bed0985b9876aa28cf57b9e046d0fee632
ShajnoskiIlija <shajnoskiilija@gmail.com>
2021-11-24 16:45:27 +0100

    Small fixes in spacing

--- spec/views/pages/home.html.erb_spec.rb
+++ spec/views/pages/home.html.erb_spec.rb
@@ -6,12 +6,13 @@ RSpec.describe 'pages/home.html.erb', type: :view do
   it 'displays titles' do
     render
 
-    expect(rendered).to include('© Copyright 2021 Ryder, All rights reserved.','Bicycles', 'Electric Scooter', 'Summer Equipment', 'Winter Equipment', 'Get Started', 'Rentals', 'Welcome', 'Contact us', 'Start here and now', 'Rentals that will fit your needs')
+    expect(rendered).to include('© Copyright 2021 Ryder, All rights reserved.', 'Bicycles', 'Electric Scooter', 'Summer Equipment', 'Winter Equipment', 'Get Started', 'Rentals', 'Welcome',
+                                'Contact us', 'Start here and now', 'Rentals that will fit your needs')
   end
 
   it 'renders contact info' do
     render
 
-    expect(rendered).to include('333-555 Radozda, Struga, Macedonia', 'Main Office', 'Tel: 222-333-222', 'Radozda, Struga', 'Here you can contact us, share your needs and  suggestions.' )
+    expect(rendered).to include('333-555 Radozda, Struga, Macedonia', 'Main Office', 'Tel: 222-333-222', 'Radozda, Struga', 'Here you can contact us, share your needs and  suggestions.')
   end
 end
