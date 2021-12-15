bd6cfc4b53cd856006d9e239eabcc826ff9f4c29
ShajnoskiIlija <shajnoskiilija@gmail.com>
2021-12-15 21:10:52 +0100

    SendGrid fixes and credentials

--- config/environment.rb
+++ config/environment.rb
@@ -5,3 +5,12 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+    domain: 'rydewithus22@gmail.com',
+    address:        "smtp.sendgrid.net",
+    port:            587,
+    authentication: :plain,
+    user_name:      'apikey',
+    password:       ENV['SENDGRID_API_KEY']
+  }
