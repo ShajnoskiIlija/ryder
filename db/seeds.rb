ab61e30ce8821d6cc66a74030f75bd4d247a1bcc
orcemaceski <orce@maceski.net>
2021-12-20 16:05:17 +0100

    Added conditon for seeds to work only on development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,26 @@ include FactoryBot::Syntax::Methods
 
 puts 'Seed started...'
 
-puts 'Creating contacts...'
-5.times do
-    create(:contact)
-end
+if Rails.env.development?
+    puts 'Creating contacts...'
+    5.times do
+        create(:contact)
+    end
 
-puts 'Creating users...'
+    puts 'Creating users...'
 
-5.times do |index|
+    5.times do |index|
 
-    user = index.zero? ? create(:user, email: 'user@test.com', first_name: 'John', last_name: 'Smith', address: 'radozda struga') : create(:user)
+        user = index.zero? ? create(:user, email: 'user@test.com', first_name: 'John', last_name: 'Smith', address: 'radozda struga') : create(:user)
 
-    rand(7..16).times { create(:rent_item, user: user) }
-end
+        rand(7..16).times { create(:rent_item, user: user) }
+    end
 
-puts 'Creating requests'
+    puts 'Creating requests'
 
-3.times do
- rand(1..4).times { create(:rent_request, rent_item: User.first.rent_items.sample,  user: User.where.not(id: User.first.id).sample) }
+    3.times do
+        rand(1..4).times { create(:rent_request, rent_item: User.first.rent_items.sample,  user: User.where.not(id: User.first.id).sample) }
+    end
 end
 
 puts 'Seeding completed!'
